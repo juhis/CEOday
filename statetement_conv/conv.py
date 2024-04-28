ee3827e18daff4e396199b3fe191b955d691be52
juhis <jkarjala@broadinstitute.org>
2024-04-28 14:40:46 +0300

    update conversion API

--- statetement_conv/conv.py
+++ statetement_conv/conv.py
@@ -1,7 +1,5 @@
 #!/usr/bin/env python3
 
-# stmt = open(sys.argv[1], 'r').readlines()
-
 import csv
 import os
 import sys
@@ -11,7 +9,7 @@ import requests
 def convert_csv(infile: str, outfile: str, api_token: str) -> str:
     """
     Write a statement csv file otherwise identical to the input file but with EUR amounts of USD transactions added
-    Use the exchangeratesapi.io API to get the daily conversion rate
+    Use the api.apilayer.com/exchangerates_data API to get the daily conversion rate
     """
     csv_in = csv.DictReader(open(infile, "r"))
     csv_out = csv.DictWriter(
@@ -24,14 +22,15 @@ def convert_csv(infile: str, outfile: str, api_token: str) -> str:
         ):  # if statement contains multiple currencies, there's more than one header
             row["Amount_EUR"] = "Amount_EUR"
         elif row["Orig currency"] == "USD" and row["Payment currency"] == "USD":
+            print(f"https://api.apilayer.com/exchangerates_data/convert?to=EUR&from=USD&amount={abs(float(row['Amount']))}&date={row['Date completed (Europe/Helsinki)']}")
             result = requests.get(
-                "http://api.exchangeratesapi.io/v1/{}".format(
-                    row["Date completed (Europe/Helsinki)"]
-                ),
-                {"access_key": api_token},
+                f"https://api.apilayer.com/exchangerates_data/convert?to=EUR&from=USD&amount={abs(float(row['Amount']))}&date={row['Date completed (Europe/Helsinki)']}",
+                {"apikey": api_token},
             )
-            rate = result.json()["rates"]["USD"]
-            row["Amount_EUR"] = "{:.2f}".format(float(row["Amount"]) / rate)
+            eur = float(result.json()["result"])
+            if float(row["Amount"]) < 0:
+                eur *= -1
+            row["Amount_EUR"] = "{:.2f}".format(eur)
         elif (
             row["Orig currency"] == "EUR" and row["Payment currency"] == "USD"
         ):  # USD-to-EUR, use the rate by which the conversion was done
